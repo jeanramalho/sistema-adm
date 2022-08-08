14890ddda5eb375d42f12d942410adda3503b2af
jeanramalho <jeanramalho.dev@gmail.com>
2022-08-08 17:31:02 -0300

    Cria classe para inserir usuário no banco de dados

---
+++ models/user.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(id, name, photo, email, phone, admin, password){
+        this.id = id,
+        this.name = name,
+        this.photo = photo,
+        this.email = email,
+        this.phone = phone,
+        this.admin = admin,
+        this.password = password
+    }
+}
