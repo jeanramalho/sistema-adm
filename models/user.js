83e4eb4e877904bf19d5d71497769496f060790e
jeanramalho <jeanramalho.dev@gmail.com>
2022-08-08 17:37:03 -0300

    inicia criação dos getters

--- models/user.js
+++ models/user.js
@@ -1,11 +1,15 @@
 class User {
     constructor(id, name, photo, email, phone, admin, password){
-        this.id = id,
-        this.name = name,
-        this.photo = photo,
-        this.email = email,
-        this.phone = phone,
-        this.admin = admin,
-        this.password = password
+        this._id = id
+        this._name = name
+        this._photo = photo
+        this._email = email
+        this._phone = phone
+        this._admin = admin
+        this._password = password
+
+        let date = new date()
+
+        this.date = date.toLocaleDateString('pt-BR')+` `+date.toLocadeTimeString('pt-BR').slice(0,5)
     }
 }
